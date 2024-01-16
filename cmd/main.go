696865375775d47e410c8b8c450327288b0b8448
sabershahhoseini <sabershahhoseini@gmail.com>
2024-01-16 18:44:30 +0330

    Added functionality to use file instead of github

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/sabershahhoseini/ipset-firewall/pkg/ipsetfw"
+	"github.com/sabershahhoseini/ipset-firewall/util/netutils"
 )
 
 func main() {
@@ -25,9 +26,13 @@ func main() {
 		fmt.Println(`You must pass an argument. Use -help for more information.
 
 Options:
+	-help					show this menu
+
 	-country	{CODE}			set country code
 	-set		{NAME}			name of ipset set
 	-check		{IP}			check if IP exists in specific country IP pool
+
+	-file		{PATH}			file path to read networks from (by default, it will be fetched from github)
 
 	-iptables				setup iptables rules
 	-policy		{POLICY}		works with -iptables and sets default policy
@@ -39,14 +44,17 @@ Example usage:
 Create a set of Iran IP pool:
 	ipsetfw -country IR -set set
 
-Create a set of Iran IP pool and block IPs from IR (Iran):
+Create a set of Iran IP pool and block IPs from Iran by adding iptables rule:
 	ipsetfw -country IR -set set -iptables -policy drop
 
-Create a set of Iran IP pool and Accpet IPs from Iran:
-	ipsetfw -country IR -set set -iptables -policy accept
+Create a set of Iran IP pool and accpet IPs from Iran by adding iptable rules with verbose mode:
+	ipsetfw -country IR -set set -iptables -policy accept -v
+
+Create a set of Iran IP pool and accpet IPs from Iran from file:
+	ipsetfw -country IR -set set -iptables -policy accept -file /opt/ips.txt
 
 Check if IP exists in IR (Iran):
-	ipsetfw go run main.go -country IR -check 1.1.1.1`)
+	ipsetfw -country IR -check 1.1.1.1`)
 		os.Exit(1)
 	}
 	set := ipsetfw.Set{
@@ -58,8 +66,10 @@ Check if IP exists in IR (Iran):
 	}
 	// If type is minio and -p is not passed, read config file from Minio and check state
 	if *countryCode != "" && *setName != "" {
-		ipsetfw.IPsetfw(set, *iptables, rule, *verbose, *file)
+		ipList := netutils.FetchIPPool(*countryCode, *verbose, *file)
+		ipsetfw.IPsetfw(ipList, set, *iptables, rule, *verbose)
 	} else if *countryCode != "" && *checkIP != "" {
-		ipsetfw.CheckIPExistsInPool(set, *checkIP, *verbose)
+		ipList := netutils.FetchIPPool(*countryCode, *verbose, *file)
+		netutils.CheckIPExistsInPool(ipList, *checkIP, *verbose)
 	}
 }
