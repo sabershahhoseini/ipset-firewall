19816ab67493464a31d03178315c6e358a535c60
sabershahhoseini <sabershahhoseini@gmail.com>
2024-01-16 18:45:07 +0330

    Changed project structure, moved some functions out of ipsetfw.go

--- pkg/ipsetfw/ipsetfw.go
+++ pkg/ipsetfw/ipsetfw.go
@@ -2,182 +2,92 @@ package ipsetfw
 
 import (
 	"fmt"
-	"io"
-	"log"
-	"net"
-	"net/http"
-	"net/netip"
 	"strings"
 	"time"
 
 	"github.com/coreos/go-iptables/iptables"
+	"github.com/sabershahhoseini/ipset-firewall/error/checkerr"
+	"github.com/sabershahhoseini/ipset-firewall/util/logger"
+	"github.com/sabershahhoseini/ipset-firewall/util/netutils"
+
 	"github.com/gmccue/go-ipset"
 )
 
-type Set struct {
-	Country string
-	SetName string
-}
-type Rule struct {
-	Policy string
-}
-
-func logger(log string, verbose bool) {
-	if verbose {
-		fmt.Println(log)
-	}
-}
-
-func fetchIPPool(url, countryCode string, verbose bool) []string {
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	client := &http.Client{}
-
-	logger("Trying to get url: "+url, verbose)
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	defer resp.Body.Close()
-
-	b, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	// fmt.Println(string(b))
-	list := strings.Split(string(b), "\n")
-	logger("Finished fetching list of IPs", verbose)
-	return list
-
-}
-
-func isCIDRValid(ip string) bool {
-	_, _, err := net.ParseCIDR(ip)
-	if err != nil {
-		return false
-	}
-	return true
-}
-
-func networkContainsIP(cidr string, ip string) bool {
-	network, err := netip.ParsePrefix(cidr)
-	if err != nil {
-		panic(err)
-	}
-
-	parsedIP, err := netip.ParseAddr(ip)
-	if err != nil {
-		panic(err)
-	}
-
-	b := network.Contains(parsedIP)
-	return b
-}
-
-func CheckIPExistsInPool(set Set, targetIP string, verbose bool) {
-	var url string
-	countryCode := set.Country
-	countryCode = strings.ToLower(countryCode)
-	url = "https://raw.githubusercontent.com/herrbischoff/country-ip-blocks/master/ipv4/" + countryCode + ".cidr"
-	ipList := fetchIPPool(url, countryCode, verbose)
-
-	for _, ip := range ipList {
-		if !isCIDRValid(ip) {
-			continue
-		}
-		if networkContainsIP(ip, targetIP) {
-			fmt.Printf("%v exists in %v\n", targetIP, ip)
-			return
-		}
-	}
-	fmt.Printf("IP %v NOT FOUND in country %v!\n", targetIP, set.Country)
-}
-
 func addIptableRule(rule Rule, setName string, verbose bool) {
 	ipt, err := iptables.New()
-	if err != nil {
-		log.Fatalln(err)
-	}
+	checkerr.Fatal(err)
 
 	rulePolicy := strings.ToUpper(rule.Policy)
 
-	logger("Adding iptables rule", verbose)
+	logger.Log("Adding iptables rule", verbose)
 	err = ipt.Insert("filter", "INPUT", 1, "-m", "set", "--match-set", setName, "src", "-j", rulePolicy)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	logger("Added iptables rule", verbose)
+	checkerr.Fatal(err)
+
+	logger.Log("Added iptables rule", verbose)
 }
 
 func removeIptableRule(rule Rule, setName string, verbose bool) {
 	ipt, err := iptables.New()
-	if err != nil {
-		log.Fatalln(err)
-	}
+	checkerr.Fatal(err)
 
-	// rulePolicy := strings.ToUpper(rule.Policy)
+	logger.Log("Removing iptables rule", verbose)
 
-	logger("Removing iptables rule", verbose)
-
-	// err = ipt.Delete("filter", "INPUT", "-m", "set", "--match-set", setName, "src")
 	err = ipt.Delete("filter", "INPUT", "-m", "set", "--match-set", setName, "src", "-j", "ACCEPT")
 	err = ipt.Delete("filter", "INPUT", "-m", "set", "--match-set", setName, "src", "-j", "DROP")
 	if err != nil {
 		if strings.Contains(err.Error(), "does a matching rule exist in that chain") {
 			return
+		} else if strings.Contains(err.Error(), "Set "+setName+" doesn't exist") {
+			return
 		}
-		log.Fatalln(err)
+		checkerr.Fatal(err)
 	}
-	logger("Removed iptables rule", verbose)
+	logger.Log("Removed iptables rule", verbose)
 }
 
-func IPsetfw(set Set, iptables bool, rule Rule, verbose bool, file string) {
+func IPsetfw(ipList []string, set Set, iptables bool, rule Rule, verbose bool) {
 
-	var url string
-	countryCode := set.Country
-	setName := set.SetName
-	countryCode = strings.ToLower(countryCode)
-	url = "https://raw.githubusercontent.com/herrbischoff/country-ip-blocks/master/ipv4/" + countryCode + ".cidr"
+	var countryCode string
+	var setName string
 
-	ipList := fetchIPPool(url, countryCode, verbose)
+	countryCode = set.Country
+	setName = set.SetName
 
 	// Construct a new ipset instance
 	ipset, err := ipset.New()
-	if err != nil {
-		log.Fatalln(err)
-	}
 
+	// If iptables argument is passed, the rule. This step is essential because
+	// We need to clear everything. And we can't remove ipset set if we don't
+	// Release it from iptables.
 	if iptables {
 		removeIptableRule(rule, setName, verbose)
 		time.Sleep(500 * time.Millisecond)
 	}
-	// Create a new set
+	// Destroy set if it exists
 	err = ipset.Destroy(setName)
 	if err != nil {
-		log.Fatalln(err)
+		if !strings.Contains(err.Error(), "The set with the given name does not exist") {
+			checkerr.Fatal(err)
+		}
 	}
+
+	// Create a new set
 	err = ipset.Create(setName, "hash:net")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	logger("Adding IPs to set", verbose)
+	checkerr.Fatal(err)
+
+	logger.Log("Adding IPs to set", verbose)
 	for _, ip := range ipList {
-		if !isCIDRValid(ip) {
+		if !netutils.IsCIDRValid(ip) {
 			continue
 		}
 		if verbose {
 			fmt.Printf("Adding %v\n", ip)
 		}
 		err := ipset.Add(setName, ip)
-		if err != nil {
-			log.Fatalln(err)
-		}
+		checkerr.Fatal(err)
+
 	}
-	logger("Added IPs to set", verbose)
+	logger.Log("Added IPs to set", verbose)
 
 	if iptables {
 		addIptableRule(rule, setName, verbose)
