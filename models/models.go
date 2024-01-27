ef33114a08a523a303e2e68ae12fbf3d7c76e856
sabershahhoseini <sabershahhoseini@gmail.com>
2024-01-27 11:18:23 +0330

    Added NOT operator to iptables config

--- models/models.go
+++ models/models.go
@@ -8,6 +8,7 @@ type Rule struct {
 	Policy string   `yaml:"policy"`
 	Insert int      `yaml:"insert"`
 	Type   []string `yaml:"type"`
+	Not    bool     `yaml:"not"`
 	Chain  string   `yaml:"chain"`
 	Table  string   `yaml:"table"`
 }
